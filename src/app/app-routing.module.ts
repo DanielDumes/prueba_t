d8a3fb07cbfeddc1d504477ca58ad27c5b2bb350
DanielDumes <136215331+DanielDumes@users.noreply.github.com>
2024-08-13 11:53:51 -0500

    app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PorquemoralesComponent } from './components/porquemorales/porquemorales
 import { ActividadesComponent } from './components/actividades/actividades.component';
 import { PlaneatuvisitaComponent } from './components/planeatuvisita/planeatuvisita.component';
 import { NuestraparroquiaComponent } from './components/nuestraparroquia/nuestraparroquia.component';
+import { RevistaComponent } from './components/revista/revista.component';
 
 const routes: Routes = [
   {path: 'inicio', component: InicioComponent},
@@ -12,6 +13,8 @@ const routes: Routes = [
   {path: 'actividades/:id', component: ActividadesComponent},
   {path: 'planeatuvisita', component: PlaneatuvisitaComponent},
   {path: 'nuestraparroquia', component: NuestraparroquiaComponent},
+  {path: 'revista', component: RevistaComponent},
+
   {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
 ];
 
@@ -20,3 +23,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
